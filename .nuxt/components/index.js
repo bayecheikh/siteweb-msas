a65754aa7932c076ee3fd1a730b8c0e326bd9990
Cheikh <bayecheikhgueye2@gmail.com>
2022-05-08 15:11:46 +0000

    Refactore components

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -7,9 +7,9 @@ export { default as OffCanvasMobileMenu } from '../..\\components\\OffCanvasMobi
 export { default as ResponsiveMobileMenu } from '../..\\components\\ResponsiveMobileMenu.vue'
 export { default as TheFooter } from '../..\\components\\TheFooter.vue'
 export { default as TheHeader } from '../..\\components\\TheHeader.vue'
+export { default as SlidersHeroSlider } from '../..\\components\\sliders\\HeroSlider.vue'
 export { default as SectionsFinancementParRegion } from '../..\\components\\sections\\FinancementParRegion.vue'
 export { default as SectionsIndicateurFinancement } from '../..\\components\\sections\\IndicateurFinancement.vue'
-export { default as SlidersHeroSlider } from '../..\\components\\sliders\\HeroSlider.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
