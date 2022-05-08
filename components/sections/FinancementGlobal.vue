ed6075d72931d81f74226de259b32ca7a05b3b78
Cheikh <bayecheikhgueye2@gmail.com>
2022-05-08 15:14:35 +0000

    Refactore components

--- components/sections/FinancementGlobal.vue
+++ components/sections/FinancementGlobal.vue
@@ -76,7 +76,7 @@
                             <div class="progress">
                                 <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>    
                             </div>
-                            <p>Engagement</p>
+                            <p>Engagement</p> 
                         </div>
                     </div>
                 </div>
